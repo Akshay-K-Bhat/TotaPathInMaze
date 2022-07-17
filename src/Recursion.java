05f8409a86428bb800e4a20f3dd5f8481c9b5acf
Akshay bhat <akshaybhat126@gmail.com>
2022-07-17 19:13:30 +0530

    Initial commit

---
+++ src/Recursion.java
@@ -0,0 +1,22 @@
+public class Recursion {
+
+    public static int printPath(int i,int j,int n,int m){
+
+        if(i==n || j==m){
+            return 0;
+        }
+        if (i==n-1 || j==m-1){
+            return 1;
+
+        }
+        int rightPaht = printPath(i+1,j,n,m);
+        int leftPath = printPath(i,j+1,n,m);
+        return rightPaht+leftPath;
+    }
+
+    public static void main(String[] args) {
+        int n = 3 ,m = 3;
+        int totalPath = printPath(1,0,n,m);
+        System.out.println(totalPath);
+    }
+}
